7db953f7b35f1d0f41cbcba9a9859584026cced5
Gloria Alcivar <sukkie_hye501@hotmail.com>
2020-04-02 01:15:37 -0500

    home screen items

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,47 +1,63 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedPage = 0;
+  final _pageOptions = [
+    Text('Item1'),
+    Text('Item2'),
+    Text('Item3'),
+    Text('Item4'),
+    Text('Item5'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bottom nav bar'),
+        title: Text('Tutor de Investigacion'),
       ),
-      body: Container(),
+      body: _pageOptions[_selectedPage],
+      drawerScrimColor: Colors.blueGrey,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
+        unselectedItemColor: Colors.blue,
+        selectedItemColor: Colors.blue,
+        currentIndex: _selectedPage,
+        onTap: (int index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.message),
             title: Text('Mensajes'),
-            backgroundColor: Colors.blueGrey,
             ),
 
             BottomNavigationBarItem(
             icon: Icon(Icons.folder),
             title: Text('Mis archivos'),
-            backgroundColor: Colors.blue,
             ),
 
             BottomNavigationBarItem(
-            icon: Icon(Icons.file_upload),
+            icon: Icon(Icons.add_circle),
             title: Text('Subir archivo'),
-            backgroundColor: Colors.blue,
             ),
 
             BottomNavigationBarItem(
             icon: Icon(Icons.search),
             title: Text('Buscar'),
-            backgroundColor: Colors.blue,
             ),
 
             BottomNavigationBarItem(
             icon: Icon(Icons.person),
             title: Text('Mi Perfil'),
-            backgroundColor: Colors.blue,
             ),
-
         ],
       ),
     );
