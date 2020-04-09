025a6806e575d8ebfe5a555d6ced112ae3113774
Gloria Alcivar <sukkie_hye501@hotmail.com>
2020-04-08 22:14:04 -0500

    appdelegate erros

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,11 +9,32 @@ import FBSDKCoreKit
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {  
+  ) -> Bool {
+        AppDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, 
-    didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+  @available(iOS 9.0, *)
+  override  func application(_ application: UIApplication, 
+    open url: URL, 
+    options: [UIApplication.OpenURLOptionsKey : Any]?) -> Bool {
+      return AppDelegate.sharedInstance().application(application,  
+        open: url, 
+        sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String, 
+        annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+    }
 
+  // iOS - 9.0
+  override func application(_ application: UIApplication, 
+    open url: URL, 
+    sourceApplication: String?, 
+    annotation: Any) -> Bool {
+      return AppDelegate.sharedInstance().application(application,
+        open: url,
+        sourceApplication: sourceApplication,
+        annotation: annotation)
+  }
+    
 }
