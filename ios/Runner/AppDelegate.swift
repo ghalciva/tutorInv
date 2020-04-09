86a9b1119e73daa07a3d58ec8bc814f9933d1125
Gloria Alcivar <sukkie_hye501@hotmail.com>
2020-04-08 21:41:31 -0500

    import modules login errors

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,8 +9,7 @@ import FBSDKCoreKit
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      
+  ) -> Bool {  
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, 
     didFinishLaunchingWithOptions: launchOptions)
